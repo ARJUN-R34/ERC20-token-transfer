62dfc0e6df8474058fe5bed60eb8add11717ad0e
ARJUN <arjun1709@gmail.com>
2020-01-08 12:29:31 +0530

    added the feature to get the address from handlename.

--- Ethereum Transaction/transaction.js
+++ Ethereum Transaction/transaction.js
@@ -1,12 +1,37 @@
+const web3 = require('web3');
 const ethers = require('ethers');
+var fs = require('fs');
+var path = require('path');
 let provider = new ethers.providers.InfuraProvider(3, "b3a845111c5f4e3eaf646c79bcb4d4c0");
 provider.apiAccessToken = '943e3d0b06c74ce2ba1442c0de5d7809';
 
+async function getAddressOfhandlename(payload) {
+    const { handlename } = payload;
+
+    const contractAddress = '0xd4680db560a9d002f0e4884bf9423753be709cdf';
+    const vendorAddress = '0x8102Eee36079E523840c57b45315e0571BFFEAC9'
+
+    var jsonPath = path.join(__dirname, 'HN contract ABI.json');
+    var abiHNRegistry = fs.readFileSync(jsonPath, 'utf8');
+
+    let MyHNContract = new ethers.Contract(contractAddress, JSON.parse(abiHNRegistry), provider);
+
+    const toAddress = await MyHNContract.resolveAddressOfHandleName(vendorAddress, web3.utils.toHex(handlename));
+    
+    return toAddress;
+}
+
 async function transaction() {
-    let privateKey = '0x80583164092334ADE24073EE7B5E51890B464C21EB5E17DE153776A6ADE7387A';
+    let privateKey;
     let wallet = new ethers.Wallet(privateKey, provider);
-    let value = '0.5';
-    let to = '0x7F36aaACbc03C055d52C7E4aFF8aAA22E2Bc306a';
+    let value;
+    let to;
+
+    //  If user enters handlename
+    let handlename;
+
+    const to = await getAddressOfhandlename({ handlename });
+    //  End
     
     const balance = await wallet.getBalance();
     console.log(balance);
